5ed3d3ba0be9bdc61f0c4a4e043ec03ae51cdabe
Henry Felipe <henry.oliveira11@etec.sp.gov.br>
2025-06-23 21:47:48 -0300

    feat: adicionando desafio que soma os numeros do 1 até o 10.

---
+++ SomaDeUmADez/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        int soma = CalcularSoma();
+
+        Thread.Sleep(500);
+        Console.Clear();
+        Console.WriteLine($"\nA soma dos números de 1 a 10 é: {soma}");
+    }
+
+    public static int CalcularSoma()
+    {
+        int soma = 0;
+
+        for (int i = 1; i <= 10; i++)
+        {
+            soma += i;
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine($"Adicionando {i}, soma parcial: {soma}");
+        }
+
+        return soma;
+    }
+}
